799a492284b91384f763b9457d60bb88a2834308
KTseng <tgpsstar@gmail.com>
2016-12-30 11:21:19 +0800

    no message

---
+++ src/app.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    // Initialize Firebase
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBPCpEc-v-mSf_riAUFd79xDzVgXXg7gN0',
+      authDomain: 'rainyday-7fa26.firebaseapp.com',
+      databaseURL: 'https://rainyday-7fa26.firebaseio.com',
+      storageBucket: 'rainyday-7fa26.appspot.com',
+      messagingSenderId: '309937705062'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ logggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            登出
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Rainyday" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
